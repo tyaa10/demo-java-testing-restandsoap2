b3b259826e54eb42483dec1c9ce62dbb4125579b
YuriiTrofimenko <tyaa@ukr.net>
2021-08-19 10:54:30 +0300

    init

---
+++ src/main/java/org/tyaa/demo/java/testing/restandsoap/Restandsoap2Application.java
@@ -0,0 +1,13 @@
+package org.tyaa.demo.java.testing.restandsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restandsoap2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Restandsoap2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/tyaa/demo/java/testing/restandsoap/Restandsoap2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tyaa.demo.java.testing.restandsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restandsoap2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
