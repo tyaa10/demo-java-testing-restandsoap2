a1cb78a4278a920a7f135177f43aedb7380789c8
YuriiTrofimenko <tyaa@ukr.net>
2021-08-19 23:49:38 +0300

    REST test cases: put n delete

--- src/test/java/org/tyaa/demo/java/testing/restandsoap/Restandsoap2ApplicationTests.java
+++ src/test/java/org/tyaa/demo/java/testing/restandsoap/Restandsoap2ApplicationTests.java
@@ -25,8 +25,11 @@ import java.util.List;
 class Restandsoap2ApplicationTests {
 
 	private static final String BASE_URL = "http://localhost:8080/api/library";
+	private static final String CORRECT_AUTHOR_NAME = "Alexander";
+	private static final String CORRECT_AUTHOR_LAST_NAME = "Pushkin";
 
 	// GET
+
 	@Test
 	@Order(3)
 	void givenGetAllAuthorsRequest_whenResponse_thenAuthorListIsNotEmpty() {
@@ -37,10 +40,11 @@ class Restandsoap2ApplicationTests {
 		assertNotEquals(0, authors.size());
 		authors.forEach(
 			a -> System.out.printf(
-				"%d %s %s\n",
+				"%d %s %s %s\n",
 				a.getAuthorId(),
 				a.getAuthorName().getFirst(),
-				a.getAuthorName().getSecond()
+				a.getAuthorName().getSecond(),
+				a.getAuthorDescription()
 			)
 		);
 	}
@@ -60,17 +64,14 @@ class Restandsoap2ApplicationTests {
 	}
 
 	// POST
+
 	@Test
 	@Order(1)
 	void givenNewAuthor_whenCreateRequest_thenCreated() throws ParseException, JsonProcessingException {
-		SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
-		LocalDate birthDate =
-			format.parse("1827.05.26").toInstant()
-				.atZone(ZoneId.systemDefault())
-				.toLocalDate();
+		LocalDate birthDate = getCorrectAuthorBirthDate();
 		AuthorDto newAuthorDto = AuthorDto.builder()
 			.authorId(getNextAuthorId())
-			.authorName(NameDto.builder().first("Alexander").second("Pushkin").build())
+			.authorName(NameDto.builder().first(CORRECT_AUTHOR_NAME).second(CORRECT_AUTHOR_LAST_NAME).build())
 			.birth(BirthDto.builder().date(birthDate).country("Russia").city("Moscow").build())
 			.authorDescription("-")
 			.nationality("Russian")
@@ -87,13 +88,9 @@ class Restandsoap2ApplicationTests {
 	@Test
 	@Order(2)
 	void givenNewAuthorWithoutId_whenCreateRequest_thenRequestFailed() throws ParseException, JsonProcessingException {
-		SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
-		LocalDate birthDate =
-			format.parse("1827.05.26").toInstant()
-				.atZone(ZoneId.systemDefault())
-				.toLocalDate();
+		LocalDate birthDate = getCorrectAuthorBirthDate();
 		AuthorDto newAuthorDto = AuthorDto.builder()
-			.authorName(NameDto.builder().first("Alexander").second("Pushkin").build())
+			.authorName(NameDto.builder().first(CORRECT_AUTHOR_NAME).second(CORRECT_AUTHOR_LAST_NAME).build())
 			.birth(BirthDto.builder().date(birthDate).country("Russia").city("Moscow").build())
 			.authorDescription("-")
 			.nationality("Russian")
@@ -107,6 +104,61 @@ class Restandsoap2ApplicationTests {
 			.assertThat().statusCode(400);
 	}
 
+	// PUT
+
+	@Test
+	@Order(5)
+	void givenAuthor_whenUpdateRequest_thenUpdated() throws JsonProcessingException {
+		AuthorDto author = getAuthorByName(CORRECT_AUTHOR_NAME + " " + CORRECT_AUTHOR_LAST_NAME);
+		author.setAuthorDescription("A Russian poet, playwright, and novelist of the Romantic era. He is considered by many to be the greatest Russian poet, and the founder of modern Russian literature.");
+			given()
+				.header("Content-Type", "application/json")
+				.body(new ObjectMapper().writeValueAsString(author))
+			.when()
+				.put(BASE_URL + "/author")
+			.then()
+				.assertThat().statusCode(200);
+	}
+
+	@Test
+	@Order(6)
+	void givenNewAuthor_whenUpdateRequest_thenNotFoundFail() throws JsonProcessingException, ParseException {
+		LocalDate birthDate = getCorrectAuthorBirthDate();
+		AuthorDto newAuthor = AuthorDto.builder()
+			.authorId(getNextAuthorId())
+			.authorName(NameDto.builder().first(CORRECT_AUTHOR_NAME).second(CORRECT_AUTHOR_LAST_NAME).build())
+			.birth(BirthDto.builder().date(birthDate).country("Russia").city("Moscow").build())
+			.authorDescription("-")
+			.nationality("Russian")
+			.build();
+		given()
+			.header("Content-Type", "application/json")
+			.body(new ObjectMapper().writeValueAsString(newAuthor))
+			.when()
+			.put(BASE_URL + "/author")
+			.then()
+			.assertThat().statusCode(404);
+	}
+
+	// DELETE
+
+	@Test
+	@Order(7)
+	void givenExistingAuthorId_whenRequestForDelete_thenDeleted() {
+		AuthorDto author = getAuthorByName(CORRECT_AUTHOR_NAME + " " + CORRECT_AUTHOR_LAST_NAME);
+		given()
+			.when().delete(BASE_URL + "/author/" + author.getAuthorId())
+			.then().assertThat().statusCode(204);
+	}
+
+	@Test
+	@Order(8)
+	void givenNotExistingAuthorId_whenRequestForDelete_thenNotFoundFail() {
+		given()
+			.when().delete(BASE_URL + "/author/" + getNextAuthorId())
+			.then().assertThat().statusCode(404);
+	}
+
 	private static Long getNextAuthorId() {
 		return given()
 					.param("pagination", "false")
@@ -117,5 +169,24 @@ class Restandsoap2ApplicationTests {
 					.getList("$", AuthorDto.class)
 					.stream()
 			.findFirst().get().getAuthorId() + 1L;
+	}
+
+	private static AuthorDto getAuthorByName(String name) {
+		return given()
+			.param("query", name)
+			.when()
+			.get(BASE_URL + "/authors/search")
+			.jsonPath()
+			.getList("$", AuthorDto.class)
+			.get(0);
+	}
+
+	private static LocalDate getCorrectAuthorBirthDate() throws ParseException {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd");
+		LocalDate birthDate =
+			format.parse("1827.05.26").toInstant()
+				.atZone(ZoneId.systemDefault())
+				.toLocalDate();
+		return birthDate;
 	}
 }
